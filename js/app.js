d91438d32e5e14f682cebf170ee3e6989cdbbc8f
eliza29 <elizaseguraf@gmail.com>
2018-03-18 19:38:04 -0500

    corrigiendo titulo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 $( document ).ready(function(){
     $('.parallax').parallax();   
-    
+})
+
+$(window).on('scroll',function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('blue-grey').removeClass('transparent');
+    }
+    else{$('nav').removeClass('blue-grey').addClass('transparent');}
 })
