af3b511635329f612ddf59784061317a7c9e25bf
eliza29 <elizaseguraf@gmail.com>
2018-04-19 19:58:34 -0500

    añadiendo subtitulos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 $( document ).ready(function(){
-    $('.parallax').parallax(); 
+    $('.parallax').parallax();
+    $('.effects').hover(function(){
+        $(this).css("color", "#6b90a1");
+        $(this).next().css({'background-color':'#6b90a1', 'width':'200px'});
+        }, function(){
+        $(this).css("color", "#455a64");
+        $(this).next().css({'background-color':'#455a64', 'width':'100px', 'transition-duration':'0.6s'});
+    });  
 })
 
 $(window).on('scroll',function(){
